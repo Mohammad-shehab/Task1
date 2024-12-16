5a87859c51d54cf26e12a669473daf43ba7d8712
Mohammad-shehab <q8mlg@outlook.com>
2024-12-16 17:03:59 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1️ C# Primitive Types
+//Task 1
+using System;
+
+int Num1 = 5;
+double Num2 = 22.5;
+string FirstName = "Mohammad";
+bool isTrue = true;
+Console.WriteLine(Num1+", "+Num2+", "+FirstName+ ", "+isTrue);
+Console.WriteLine("------------------------------");
+
+// Task 2
+int number = 5; 
+double result = Convert.ToDouble(number);
+Console.WriteLine(number.GetType());
+Console.WriteLine(result.GetType());
+//only the data type of result is set to double; using convert does not change the variable data type it only changes to the new type for the new variable we want to store it in.
+Console.WriteLine("------------------------------");
+
+
+//Bonus!
+// the char data type can only be used for one character {digit,letter or symbol} inside single 
+
+
+// 2 C# Variables
+//Task 1
+string studentName = "Mohammad";
+double grade_point_average = 99.9;
+string favorite_subject = "Math";
+
+
+
+
+//Task 2
+double width = 5.5; 
+double height = 3.2;
+double Area = width * height;
+Console.WriteLine("The area of the rectangle is "+ Area);
+Console.WriteLine("------------------------------");
+
+//challenge 1 
+
+Console.WriteLine("Enter hour: ");
+string lunch_hour = Console.ReadLine();
+int time_for_lunch = Convert.ToInt32(lunch_hour);
+if(time_for_lunch>= 11 && time_for_lunch <=14 )
+{
+    Console.WriteLine("Lunch Time");
+}
+else
+{
+    Console.WriteLine("Not Yet");
+}
